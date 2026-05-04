8aae067403d6796bef535ecf7153774c81f6df01
Satya Chekuri <sai@mac.mynetworksettings.com>
2026-05-03 22:24:10 -0400

    Fix CSS inclusion and 404 redirect for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
 export default defineConfig({
-  base: '/veda-rangapravesam/',
+  base: '/',
   plugins: [
     TanStackRouterVite(),
     react(),
